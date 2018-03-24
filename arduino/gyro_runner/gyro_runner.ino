15e5c5bdee2c4491a651a4d25f964f7137ebcd54
Michael Plazek <michael.plazek91@gmail.com>
2018-03-24 18:31:43 -0400

    arduino code

---
+++ arduino/gyro_runner/gyro_runner.ino
@@ -0,0 +1,67 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM9DS0.h>
+
+Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(1000);  // Use I2C, ID #1000
+sensors_event_t accel, mag, gyro, temp;
+
+#define LSM9DS0_XM_CS 10
+#define LSM9DS0_GYRO_CS 9
+#define LSM9DS0_SCLK 13
+#define LSM9DS0_MISO 12
+#define LSM9DS0_MOSI 11
+
+void configureSensor(){
+
+  lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
+  lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
+  lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
+}
+
+void setup() {
+  
+#ifndef ESP8266
+  while (!Serial); 
+#endif
+
+  Serial.begin(9600);
+  
+  if(!lsm.begin()){
+    Serial.println("ERROR: no LSM9DS0 detected");
+    while(1);
+  }
+  
+  configureSensor();
+}
+
+void loop() {  
+
+  waitForResponse();
+  sendGyroscopeData();
+}
+
+void sendGyroscopeData() {
+
+  lsm.read();
+  lsm.getEvent(&accel, &mag, &gyro, &temp); 
+
+  String data = "";
+
+  data.concat(gyro.gyro.x);
+  data.concat(",");
+  data.concat(gyro.gyro.y);
+
+  Serial.println(data);
+}
+
+void waitForResponse(){
+
+  int response = 0;
+  while(response != 1) {
+    response = Serial.read();
+  }
+
+  delay(50);
+}
+
